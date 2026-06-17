f7af01730c94f68da911657f593e01f28c7d2f0c
luoyeスキ <100058663+luoye520ww@users.noreply.github.com>
2026-06-17 20:24:58 +0800

    Allow comments to store approval status

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['name', 'email', 'content', 'image_path'];
+    protected $fillable = ['name', 'email', 'content', 'image_path', 'is_approved'];
+
+    protected $casts = [
+        'is_approved' => 'boolean',
+    ];
 
     public function post() {
         return $this->belongsTo(Post::class);
